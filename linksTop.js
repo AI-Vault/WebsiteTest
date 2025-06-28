d6ec31ffa536814bf97a79aad499c76e38b1ac2e
AI-Vault <55486896+AI-Vault@users.noreply.github.com>
2025-06-28 14:20:25 +0200

    Add files via upload

---
+++ linksTop.js
@@ -0,0 +1,7 @@
+// Bovenste linksarray in extern bestand
+const linksTop = [
+  {kleur:2,tekst:"New AI lists",url:"indexNews.html",beschrijving:""},
+  {kleur:2,tekst:"Learn",url:"indexLearn.html",beschrijving:""},
+  {kleur:2,tekst:"Home Made",url:"indexHomeMade.html",beschrijving:""},
+  {kleur:2,tekst:"Events",url:"indexEvents.html",beschrijving:""},
+];
