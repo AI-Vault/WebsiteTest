264481f9d098ec0fa8648985f06b6817a4363fac
AI-Vault <55486896+AI-Vault@users.noreply.github.com>
2025-06-29 08:26:10 +0200

    Update linksTop.js

--- linksTop.js
+++ linksTop.js
@@ -6,3 +6,11 @@ const linksTop = [
   {kleur:2,tekst:"Events",url:"indexEvents.html",beschrijving:""},
   {kleur:2,tekst:"Kranten",url:"https://ai-vault.github.io/WebsiteTest/kranten/indexKrant.html",beschrijving:""},
 ];
+
+const linksTop = [
+  { kleur: 2, tekst: "New AI lists",url: "indexNews.html",beschrijving: "", target: "tabNewAI"},
+  { kleur: 2, tekst: "Learn",url: "indexLearn.html",beschrijving: "", target: "tabLearn"},
+  { kleur: 2, tekst: "Home Made",url: "indexHomeMade.html",beschrijving: "", target: "tabHomeMade"},
+  { kleur: 2, tekst: "Events",url: "indexEvents.html",beschrijving: "", target: "tabEvents"},
+  { kleur: 2, tekst: "Kranten",url: "https://ai-vault.github.io/WebsiteTest/kranten/indexKrant.html",beschrijving: "", target: "tabKranten"},
+];
