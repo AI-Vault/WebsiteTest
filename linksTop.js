3316feb03e32afada564d2b23d0cf71eec55714a
AI-Vault <55486896+AI-Vault@users.noreply.github.com>
2025-06-29 08:30:42 +0200

    Update linksTop.js

--- linksTop.js
+++ linksTop.js
@@ -1,13 +1,5 @@
 // Bovenste linksarray in extern bestand
 const linksTop = [
-  {kleur:2,tekst:"New AI lists",url:"indexNews.html",beschrijving:""},
-  {kleur:2,tekst:"Learn",url:"indexLearn.html",beschrijving:""},
-  {kleur:2,tekst:"Home Made",url:"indexHomeMade.html",beschrijving:""},
-  {kleur:2,tekst:"Events",url:"indexEvents.html",beschrijving:""},
-  {kleur:2,tekst:"Kranten",url:"https://ai-vault.github.io/WebsiteTest/kranten/indexKrant.html",beschrijving:""},
-];
-
-const linksTop = [
   { kleur: 2, tekst: "New AI lists",url: "indexNews.html",beschrijving: "", target: "tabNewAI"},
   { kleur: 2, tekst: "Learn",url: "indexLearn.html",beschrijving: "", target: "tabLearn"},
   { kleur: 2, tekst: "Home Made",url: "indexHomeMade.html",beschrijving: "", target: "tabHomeMade"},
